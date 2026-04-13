c4cbdba13e580abf86a7bb6b39edbec573eab855
opi-leonardo <leonardo.corti@opiquad.it>
2026-04-13 13:59:04 +0200

    eliminazione dashboard vuoti

--- resources/js/pages/client/dashboard.tsx
+++
@@ -1,3 +0,0 @@
-export default function ClientDashboard() {
-    return <h1>Benvenuto! Questa è la tua bacheca allenamenti.</h1>;
-}
--- resources/js/pages/pt/dashboard.tsx
+++
@@ -1,3 +0,0 @@
-export default function PTDashboard() {
-    return <h1>Benvenuto Trainer! Qui gestirai i tuoi clienti.</h1>;
-}
